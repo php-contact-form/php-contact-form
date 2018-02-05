f0a6f1875c8c106832f506da4319d551053f1044
PHP Contact Form <apps@ceilicornelison.com>
2018-02-04 21:28:55 -0600

    Server side form field validation & tests

--- src/Contact.php
+++ src/Contact.php
@@ -2,8 +2,36 @@
 
 class Contact
 {
+    private $validationErrors = [];
+
     public function render()
     {
         return file_get_contents('public/index.php');
+    }
+
+    // Only current validation condition is not null
+    // as per project requirements
+    public function validateField($condition = 'NotNull', $fieldName, $fieldValue = '')
+    {
+        $validationMethod = 'validate' . $condition;
+
+        if (true !== $this->$validationMethod($fieldValue)) {
+            $this->setValidationErrorOnField($fieldName, $condition);
+        }
+    }
+
+    public function getValidationErrors()
+    {
+        return $this->validationErrors;
+    }
+
+    private function setValidationErrorOnField($fieldName, $condition)
+    {
+        $this->validationErrors[$fieldName] = $condition;
+    }
+
+    private function validateNotNull($value)
+    {
+        return !empty($value);
     }
 }
--- tests/ContactTest.php
+++ tests/ContactTest.php
@@ -21,4 +21,33 @@ class ContactTests extends PHPUnit_Framework_TestCase
         $result = $this->contact->render();
         $this->assertContains('Contact Guy Smiley', $result);
     }
+
+    public function testValidationErrorsInitedAsEmptyArray()
+    {
+        $validationErrors = $this->contact->getValidationErrors();
+
+        $this->assertEquals($validationErrors, []);
+        $this->assertEmpty($validationErrors);
+    }
+
+    public function testNullFieldValueIsProperlyValidated()
+    {
+        $this->contact->validateField('NotNull', 'name', '');
+
+        $validationErrors = $this->contact->getValidationErrors();
+
+        $this->assertNotEmpty($validationErrors);
+        $this->assertCount(1, $validationErrors);
+        $this->assertArrayHasKey('name', $validationErrors);
+        $this->assertEquals($validationErrors['name'], 'NotNull');
+    }
+
+    public function testNotNullFieldValueIsProperlyValidated()
+    {
+        $this->contact->validateField('NotNull', 'name', 'George');
+
+        $validationErrors = $this->contact->getValidationErrors();
+
+        $this->assertEmpty($validationErrors);
+    }
 } 
